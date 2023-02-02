013c17d752959e63c6793a30b599f5798ecb4296
Emre <kozanfurkanemre@gmail.com>
2023-02-02 20:28:11 +0300

    Bug fix

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,17 +1,13 @@
 import "../../styles/globals.css"
-import { SessionProvider } from 'next-auth/react';
 
-import { Open_Sans } from "@next/font/google";
-const open = Open_Sans({
-  weight: "400",
-});
+
 
 export default function RootLayout({ children }) {
   
   return (
     <html lang="tr">
       <head />
-      <body className={`min-h-screen ${open.className} s overflow-y-auto lg:grid lg:grid-cols-5  w-full flex flex-col  `}>
+      <body className={`min-h-screen  s overflow-y-auto lg:grid lg:grid-cols-5  w-full flex flex-col  `}>
         
        
         <div className="lg:col-span-4">
