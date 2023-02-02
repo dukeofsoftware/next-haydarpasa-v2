812f71391af4f263a839119583a4cb42b3fab850
Emre <kozanfurkanemre@gmail.com>
2023-02-02 20:29:22 +0300

    Auth eklendi

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,28 +1,40 @@
 
 import '@/styles/globals.css';
 import Sidebar from "@/ui/dashboard/sidebar";
-
-
+import { getServerSession } from "next-auth/next"
+import authOptions from "pages/api/auth/[...nextauth]"
+import Link from "next/link";
+import {redirect} from "next/navigation"
 interface IProps {
   children: React.ReactNode;
 }
 export default async function RootLayout({
   children
 }: IProps) {
-
+  const session = await getServerSession(authOptions)
+  if(!session){
+    redirect("/auth")
+  }
   return (
     <html>
       <head>
         <title>Dashboard</title>
       </head>
       <body className="overflow-y-scroll flex ">
-        <Sidebar/>
-        <main className='min-h-screen flex-1'>
-        {children}
 
-        </main>
+        {session ? (
+          <>
+            <Sidebar />
+            <main className='min-h-screen flex-1'>
+              {children}
+            </main>
+          </>
+        ) :
+          <Link href="/auth">Giriş Yap</Link>}
 
-        </body>
+
+
+      </body>
     </html>
   );
 }
