7e14461a3b5cfb80ebb6570f71ab077a2244646e
Emre <kozanfurkanemre@gmail.com>
2023-02-03 21:16:45 +0300

    Cizerge yapıldı

---
+++ app/(client)/akademik/cizerge/constants.ts
@@ -0,0 +1,51 @@
+export const cizerges = [
+  {
+    id: '1',
+    ders: '1.Ders',
+    giris: '8.00',
+    cikis: '8.40',
+  },
+
+  {
+    id: '2',
+    ders: '2.Ders',
+    giris: '8.50',
+    cikis: '9.30',
+  },
+  {
+    id: '3',
+    ders: '3.Ders',
+    giris: '9.40',
+    cikis: '10.20',
+  },
+  {
+    id: '4',
+    ders: '4.Ders',
+    giris: '10.30',
+    cikis: '11.10',
+  },
+  {
+    id: '5',
+    ders: '5.Ders',
+    giris: '11.20',
+    cikis: '12.00',
+  },
+  {
+    id: '6',
+    ders: '6.Ders',
+    giris: '12.40',
+    cikis: '13.20',
+  },
+  {
+    id: '7',
+    ders: '7.Ders',
+    giris: '13.30',
+    cikis: '14.10',
+  },
+  {
+    id: '8',
+    ders: '8.Ders',
+    giris: '14.20',
+    cikis: '15.00',
+  },
+];
---
+++ app/(client)/akademik/cizerge/page.tsx
@@ -0,0 +1,43 @@
+import { cizerges } from './constants';
+import SectionTitle from '@/ui/client/global/SectionTitle';
+async function page() {
+  return (
+    <section className="min-h-screen w-full bg-white px-[10%] pt-12">
+      <SectionTitle>Haydarpaşa lisesi ders giriş çıkış saatleri</SectionTitle>
+
+      <div className="relative overflow-x-auto">
+        <table className="border-primary w-full rounded-lg border-2  text-left text-sm text-gray-500">
+          <thead className="bg-primary-5 py-2 text-xs uppercase text-white  ">
+            <tr>
+              <th scope="col" className="py-3 px-6 text-[16px] font-bold">
+                Ders
+              </th>
+              <th scope="col" className="py-3 px-6  text-[16px] font-bold">
+                Giriş
+              </th>
+              <th scope="col" className="py-3 px-6  text-[16px] font-bold">
+                Çıkış
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {cizerges.map((cizerge) => (
+              <tr key={cizerge.id} className="border-b bg-white ">
+                <th
+                  scope="row"
+                  className="whitespace-nowrap py-4 px-6 text-lg font-medium text-gray-900  "
+                >
+                  {cizerge.ders}
+                </th>
+                <td className="py-4 px-6">{cizerge.giris}</td>
+                <td className="py-4 px-6">{cizerge.cikis}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+}
+
+export default page;
