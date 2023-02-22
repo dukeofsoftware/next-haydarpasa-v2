28718e1a4cd2984d449ca5da7c5479dbe423b7d0
Emre <kozanfurkanemre@gmail.com>
2023-02-22 19:11:52 +0300

    refactor: yeni stiller eklendi

--- ui/client/home/hero/HeroBody.tsx
+++ ui/client/home/hero/HeroBody.tsx
@@ -2,7 +2,7 @@ interface HeroBodyProps {
   children: React.ReactNode;
 }
 const HeroBody = ({ children }: HeroBodyProps) => {
-  return <p className="mb-12 text-lg text-gray-500  ">{children}</p>;
+  return <p className="mb-12 text-base sm:text-lg text-gray-500  ">{children}</p>;
 };
 
 export default HeroBody;
