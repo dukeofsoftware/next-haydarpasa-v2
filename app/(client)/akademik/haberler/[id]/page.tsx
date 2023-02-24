accb0b536b71b18734d574c89b5bf7595c913547
Emre <kozanfurkanemre@gmail.com>
2023-02-24 20:04:17 +0300

    Fix:generate static params fixed

--- app/(client)/akademik/haberler/[id]/page.tsx
+++ app/(client)/akademik/haberler/[id]/page.tsx
@@ -1,8 +1,10 @@
 import Post from './Post';
 import { getPostById, getPosts } from '@/lib/prisma/news';
+import { Post as PostType  } from '@/types/PostTypes';
 
-async function page({ id }: any) {
-  const { post } = await getPostById(id);
+async function page({ params }: any) {
+  const {id} = params;  
+  const { post } = await getPostById(id || params.id);
 
   return <section>
     {post ? <Post post={post} /> : <div>Post not found</div>
@@ -13,5 +15,8 @@ async function page({ id }: any) {
 export default page;
 export async function generateStaticParams() {
   const { posts } = await getPosts();
-  return posts && posts.map((post: any) => ({ params: { id: post.id } }));
+  if(!posts) return [];
+  return posts.map((post:PostType)=>({
+    id: post.id.toString()
+  }))
 }
