3d37f4dad82e3ce1f65b7c81862251df5f0b2a5f
Emre <kozanfurkanemre@gmail.com>
2023-02-26 15:37:18 +0300

    refactor:format

--- app/(client)/akademik/haberler/[id]/page.tsx
+++ app/(client)/akademik/haberler/[id]/page.tsx
@@ -1,22 +1,21 @@
 import Post from './Post';
 import { getPostById, getPosts } from '@/lib/prisma/news';
-import { Post as PostType  } from '@/types/PostTypes';
+import { Post as PostType } from '@/types/PostTypes';
 
 async function page({ params }: any) {
-  const {id} = params;  
+  const { id } = params;
   const { post } = await getPostById(id || params.id);
 
-  return <section>
-    {post ? <Post post={post} /> : <div>Post not found</div>
-    }
-  </section>
+  return (
+    <section>{post ? <Post post={post} /> : <div>Post not found</div>}</section>
+  );
 }
 
 export default page;
 export async function generateStaticParams() {
   const { posts } = await getPosts();
-  if(!posts) return [];
-  return posts.map((post:PostType)=>({
-    id: post.id.toString()
-  }))
+  if (!posts) return [];
+  return posts.map((post: PostType) => ({
+    id: post.id.toString(),
+  }));
 }
