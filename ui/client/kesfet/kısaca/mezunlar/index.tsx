7a025a53b080d3afa58fcb1cd4ef2826daaa1652
Emre <kozanfurkanemre@gmail.com>
2023-03-01 18:06:50 +0300

    fix:text'ler fixlendi

--- ui/client/kesfet/kısaca/mezunlar/index.tsx
+++ ui/client/kesfet/kısaca/mezunlar/index.tsx
@@ -1,14 +1,14 @@
+import  SectionTitle  from '@/ui/client/global/SectionTitle';
+
 const MezunlarSection = () => {
   return (
     <section>
       <div className="my-6 flex flex-col gap-4">
-        <h1 className="text-primary text-2xl font-light">
-          Mezunlarımızı tanıyabilirsiniz.
-        </h1>
-        <p className="mb-5 leading-relaxed text-gray-600">
+        <SectionTitle>Mezunlarımız</SectionTitle>
+        <p className="mb-5 leading-relaxed text-[0.6rem] xs:text-sm sm:text-base text-gray-600">
           Haydarpaşa ismini daha önce duyduysanız muhtemelen mezunlarımızıda
-          duymuşsunuzdur.Haydarpaşa Lisesi başarılı insanlar
-          yetiştirmektedir.Muhtemeln en azından birkaçının ismini daha önce
+          duymuşsunuzdur. Haydarpaşa Lisesi başarılı insanlar
+          yetiştirmektedir. Muhtemeln en azından birkaçının ismini daha önce
           duymuşsunuzdur.
         </p>
         <div className="flex flex-wrap gap-4"></div>
