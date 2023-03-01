ae8479e5d8cc259c8a9f6a44287a22246096ddc7
Emre <kozanfurkanemre@gmail.com>
2023-03-01 18:04:07 +0300

    small menu'deki content ana navbara taşındı

--- ui/client/global/navbar/sidebar/SmallMenu.tsx
+++ ui/client/global/navbar/sidebar/SmallMenu.tsx
@@ -1,66 +1,30 @@
 'use client';
-import { logolar } from '@/constants/okul_logoları';
 
-import { AiOutlineClose } from 'react-icons/ai';
-import { IconContext } from 'react-icons';
+
 import { links } from './constants';
-import Image from 'next/image';
-import Link from 'next/link';
 import Accordion from '../accordion';
+import { memo } from 'react';
 
 interface SmallMenuProps {
   isOpen: boolean;
-  setIsOpen: (state: boolean) => void;
 }
 
-function SmallMenu({ isOpen, setIsOpen }: SmallMenuProps) {
+function SmallMenu({ isOpen }: SmallMenuProps) {
+
   return (
-    <div>
-      <div
-        className={`fixed z-40 flex max-h-[90px] w-full items-center justify-between  px-[4%] py-4 md:hidden ${
-          isOpen ? 'visible' : ' invisible'
-        }`}
-      >
-        <Link
-          href="/"
-          className="relative block h-[80px] w-[80px] flex-none  md:hidden "
-        >
-          <Image src={logolar.beyazHaydarpasa} alt="okul logosu" />
-        </Link>
-        <div
-          className={`flex h-full w-full   items-center justify-end ${
-            isOpen ? '' : ''
-          } `}
-        >
-          <button
-            onClick={() => {
-              setIsOpen(!isOpen);
-            }}
-            className={`in-ease-out flex max-w-[150px]  items-center justify-center rounded-full bg-primary-3	 p-3  transition-all duration-300 active:bg-red-800 md:w-full md:gap-2 md:py-3 ${
-              isOpen ? ' bg-primary-3' : ''
-            }`}
-          >
-            <div
-              className={`in-ease-out duration-300  ${
-                isOpen ? 'rotate-0 opacity-100 ' : 'rotate-180 opacity-0 '
-              }`}
-            >
-              <IconContext.Provider value={{ color: '' }}>
-                <AiOutlineClose size={24} fill="white" />
-              </IconContext.Provider>
-            </div>
-          </button>
-        </div>
-      </div>
+    <>
+
+
       {isOpen && (
-        <div className="min-w-screen z-50 flex h-full min-h-screen w-full flex-col bg-red-800 bg-[length:200px_100px] px-[4%] pt-[5rem] duration-500 ease-out md:hidden  ">
+        <div className="  max-w-screen z-30 flex h-full min-h-screen w-full flex-col bg-red-800 bg-[length:200px_100px] px-[2%] sm:px-[4%] pt-[5rem] duration-500 ease-out md:hidden  ">
           {links.map((link, index) => (
             <Accordion key={index} main={link.main} links={link.links} />
           ))}
         </div>
+
       )}
-    </div>
+    </>
   );
 }
 
-export default SmallMenu;
+export default memo(SmallMenu);
