9f8f8ddbc4beee93eb7dac7543253dee672f2469
Emre <kozanfurkanemre@gmail.com>
2023-03-01 18:07:57 +0300

    fix: itemlerin 0'dan başlama sorunu düzeltildi

--- ui/client/kesfet/kısaca/landing/index.tsx
+++ ui/client/kesfet/kısaca/landing/index.tsx
@@ -4,7 +4,7 @@ const Landing = () => {
   return (
     <section className="mt-12">
       {listElements.map((item, idx) => (
-        <ListElement title={item} index={idx + 1} key={item} />
+        <ListElement title={item} index={idx} key={item} />
       ))}
     </section>
   );
